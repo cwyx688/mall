5f293015950c428e88c45381ff7fe2310620f1f2
xjcvv <20050374@qq.com>
2019-12-19 22:26:14 +0800

    请求数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,47 +3,84 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    
+
     <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends "></recommend-view>
-    
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature></feature>
+    <tab-control
+      :titles="['流行', '新款', '精选']"
+      class="tab-control"
+    ></tab-control>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar.vue'
-  import HomeSwiper from './childComps/HomeSwiper.vue'
-  import RecommendView from './childComps/RecommendView.vue'
-  import {getHomeMultidata} from 'network/home'
-  
+import NavBar from "components/common/navbar/NavBar.vue";
+import HomeSwiper from "./childComps/HomeSwiper.vue";
+import RecommendView from "./childComps/RecommendView.vue";
+import Feature from "./childComps/FeatureView.vue";
+import TabControl from "components/content/tabControl/TabControl.vue";
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
-  export default {
-    components:{
-      NavBar,
-      HomeSwiper,
-      RecommendView
-    },
-    data() {
-      return {
-        banners:[],
-        recommends:[]
+export default {
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    Feature,
+    TabControl
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
       }
-    },
-    created() {
-      getHomeMultidata().then(res=>{
-        
+    };
+  },
+  created() {
+    this.getHomeMultidata()
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list;
-              console.log(res);
-              
+        console.log(res);
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then(res=>{
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
       })
     }
-  } 
+  }
+};
 </script>
 
 <style scoped>
-  .home-nav {
-    background-color: var(--color-tint);
-    color:#fff;
-  }
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 1;
+}
+#home {
+  padding-top: 44px;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
+}
 </style>
