a81307466cb41364c433b14238ff582a429bdd37
xjcvv <20050374@qq.com>
2019-12-29 15:32:00 +0800

    go

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import toast from 'components/common/toast'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.use(toast)
+Vue.use(VueLazyLoad, {
+  // 这里可以设置很多属性，具体看文档
+  loading:require('assets/img/common/placeholder.png')
+})
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
